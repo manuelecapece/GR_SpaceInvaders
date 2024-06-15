4457e39ad2686ae0ba70550495102ccc5e6a7a2a
gianfranco-manfreda <74355508+gianfranco-manfreda@users.noreply.github.com>
2024-06-15 16:24:15 +0200

    obj alieno1 modificato

--- spaceInvaders/Assimp, Hello World!/main.cpp
+++ spaceInvaders/Assimp, Hello World!/main.cpp
@@ -531,9 +531,9 @@ unsigned int loadTexture3(char const* path, bool gammaCorrection)
 
 int main()
 {
-	bool schermoIntero = false;
+	bool schermoIntero = true;
 
-	vista = 0;
+	vista = 1;
 
 	if (vista == 0) {
 		//Vista isometrica frontale dall'alto
